3bdb775e80babe0e90b7b8a21d208e17d1ad9440
Maximo Bautista <max.bautista97@gmail.com>
2019-02-20 00:37:21 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -9,6 +9,7 @@ class Application
     if req.path.match(/items/) != nil
       search_param = req.path.split("/items/").last
       item = Item.all.select { |item| item.name == search_param}
+<<<<<<< HEAD
       #binding.pry
       if item.count > 0 
         resp.write item.first.price
@@ -18,6 +19,15 @@ class Application
       end
     else
       resp.status = 404
+=======
+      binding.pry
+      if item != nil
+        item.price
+      else
+        resp.write "Item not found"
+      end
+    else
+>>>>>>> 38b2a326f352d79c7f55fa4d7ad019fbad8c845d
       resp.write "Route not found"
     end
     
